6818d7719fc2a72659c8ce640a22ddeb694b0dd2
Roxanedu1027 <62690848+Roxanedu1027@users.noreply.github.com>
2021-12-17 15:52:33 +0100

    Update beautifuljekyll.js

--- assets/js/beautifuljekyll.js
+++ assets/js/beautifuljekyll.js
@@ -148,12 +148,14 @@ function showSlides(n) {
 
 
 var slideIndex = 0;
-showSlides();
+showSlides(slideIndex);
 
-function showSlides() {
+function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
@@ -164,7 +166,7 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
 
