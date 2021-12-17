745b4a3a37cc13d00fd21ca0171d69953569f7bf
Roxanedu1027 <62690848+Roxanedu1027@users.noreply.github.com>
2021-12-17 15:59:57 +0100

    Update beautifuljekyll.js

--- assets/js/beautifuljekyll.js
+++ assets/js/beautifuljekyll.js
@@ -114,10 +114,10 @@ var BeautifulJekyllJS = {
 
 // SlideShows ???
 
-/*
+
 var slideIndex = 1;
 showSlides(slideIndex);
-*/
+
 
 // Next/previous controls
 function plusSlides(n) {
@@ -129,7 +129,6 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-/*
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
@@ -144,31 +143,7 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-}*/
-
-
-var slideIndex = 0;
-showSlides(slideIndex);
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
-
 
 
 // 2fc73a3a967e97599c9763d05e564189
